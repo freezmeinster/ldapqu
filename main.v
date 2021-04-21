6a6ec26320957c14eec08a2a9584bbf3ded5ff2a
bramandityo <freez.meinster@gmail.com>
2021-04-22 01:20:46 +0700

    test1

--- main.v
+++ main.v
@@ -1,7 +1,13 @@
+module main
+
 import vweb
+import os
+import time
 
 struct App {
     vweb.Context
+    port    int
+    timeout int
 }
 
 
@@ -13,5 +19,26 @@ fn (mut app App) hello_user(user string) vweb.Result {
 }
 
 fn main() {
-	vweb.run<App>(8080)
+    if os.args.len != 2 {
+        panic('Usage: `ldapqu PORT`')
+    }
+    http_port := os.args[1].int()
+    mut app := App{
+        port: http_port
+        timeout: 10
+    }
+	vweb.run_app<App>(mut app, http_port)
+}
+
+pub fn (mut app App) init() {
+}
+
+pub fn (mut app App) init_once() {
+	eprintln('>> webserver: started on http://127.0.0.1:$app.port/ , with maximum runtime of $app.timeout milliseconds.')
+}
+
+fn (mut app App) gracefull_exit() {
+	eprintln('>> webserver: gracefull_exit')
+	time.sleep(10)
+	exit(0)
 }
